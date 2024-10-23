83654803829965602ebb6fd40172a51e0a92e7ce
Volodymyr Kameniev <vladimirkamenev22@gmail.com>
2024-10-23 22:39:09 +0300

    test2

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+
+	char bracket;
+
+	cin >> bracket;
+
+	switch (bracket) {
+	case '(':cout << "відкрита кругла дужка"; break;
+	case ')':cout << "закрита кругла дужка "; break;
+	case '[':cout << "відкрита квадратна дужка"; break;
+	case ']':cout << "закрита квадратна дужка"; break;
+	case '{':cout << "відкрита фігурна дужка"; break;
+	case '}':cout << "закрита фігурна дужка"; break;
+	default:cout << "Не дужка";
+	}
+}
